1ee2ac3b0c11a5e557a3906c24c18cdea113ea77
Tasin Ahmed <atasin1998@gmail.com>
2024-03-19 16:03:13 -0400

    Add friends route

---
+++ app/api/friends/[uid]/route.js
@@ -0,0 +1,16 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../auth/[...nextauth]/route';
+import prisma from '../../../libs/prisma';
+
+export async function GET(req, { params: uid }) {
+  console.log(authOptions);
+  const session = await getServerSession(authOptions);
+  const friends = await prisma.friend.findMany({
+    where: {
+      requesterId: session.user.id,
+      status: 'ACCEPTED',
+    },
+  });
+  console.log(friends);
+  return Response.json({ success: true });
+}
